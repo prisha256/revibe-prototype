83262fcc905921d5745507d9d0bd596077522c48
Prisha <prishakhairnar40@gmail.com>
2025-06-27 10:44:52 +0530

    fix: correct postcss configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,7 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
+// The CORRECT postcss file
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
